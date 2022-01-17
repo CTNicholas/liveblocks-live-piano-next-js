170fc06c9ea7860a5417926b9560f32c2d80bd2d
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2022-01-17 13:54:52 +0000

    fix: Fixed invalid crossOrigin attribute

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home ({
           href="/favicon-16x16.png"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
       {hasSetupLiveblocksKey ? (
