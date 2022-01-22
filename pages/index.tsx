56cc820a053da437b8c68fba0ecbc5770ca65e30
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2022-01-22 11:16:42 +0000

    fix: Meta tag added by mistake

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,7 +62,6 @@ export default function Home ({
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-        <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#282830" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="twitter:card" content="summary_large_image" />
