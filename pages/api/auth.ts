1de2d72e582acbc328de3e318248826a45c4ab6d
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-11-27 13:17:39 +0000

    feat: Update Liveblocks

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,7 +1,11 @@
-import { authorize } from '@liveblocks/node'
+import { Liveblocks } from '@liveblocks/node'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const API_KEY = process.env.LIVEBLOCKS_SECRET_KEY
+const API_KEY = process.env.LIVEBLOCKS_SECRET_KEY as string
+
+const liveblocks = new Liveblocks({
+  secret: API_KEY,
+})
 
 export default async function auth (req: NextApiRequest, res: NextApiResponse) {
   if (!API_KEY) {
@@ -11,16 +15,23 @@ export default async function auth (req: NextApiRequest, res: NextApiResponse) {
   // For the avatar example, we're generating random users
   // and set their info from the authentication endpoint
   // See https://liveblocks.io/docs/api-reference/liveblocks-node#authorize for more information
-  const response = await authorize({
-    room: req.body.room,
-    secret: API_KEY,
-    userInfo: {
+  const user = {
+    id: Math.random().toString(36).slice(-6),
+    info : {
       name: NAMES[Math.floor(Math.random() * NAMES.length)],
       color: COLORS[Math.floor(Math.random() * COLORS.length)],
       picture: `/assets/avatars/${Math.floor(Math.random() * 10)}.png`
     }
-  })
-  return res.status(response.status).end(response.body)
+  }
+
+  const session = liveblocks.prepareSession(
+    user.id,
+    { userInfo: user.info }
+  )
+
+  session.allow(req.body.room, session.FULL_ACCESS)
+  const { status, body } = await session.authorize()
+  return res.status(status).end(body)
 }
 
 const COLORS = [
